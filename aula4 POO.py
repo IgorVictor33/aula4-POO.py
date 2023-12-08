10c54a2085a24df9eeb9354789af18dcaddd4c87
IgorVictor33 <152664824+IgorVictor33@users.noreply.github.com>
2023-12-08 20:08:45 -0300

    Update aula4 POO.py

--- aula4 POO.py
+++ aula4 POO.py
@@ -1,66 +1,61 @@
 class Carro:
 
-    def __init__(self, cor, marca, modelo):
-        self.cor = cor
-        self.marca = marca
-        self.modelo = modelo
-        self.ligado = False
-        self.andando = False
+  def __init__(self, cor, marca, modelo, ligado=False, andando= False):
 
-    def ligar(self):
-       if self.ligado == True:
-        print(motor , 'ligado!')
-       else:
-        print('desligado!')
-       if self.ligado == False:
-        print('esta parado!')
-       else:
-        print('esta andando!')  
-       
-
-    def desligar(self):
-       if self.ligado:
-        print(f'o carro ja esta ligado!')
-
-       if self.ligado == True:
-        print(motor, 'desligado!')
-        return
-
-    
-    def andar(self):
-
-       if self.andando == True:
-        print(movimento, 'andando!')
-       if self.status == False:
-        print(motor, 'esta ligado!')
-        return
-
-
-    def parar(self):
-
-      if self.andando:
-        print(movimento, 'andando!')
-      return
-
-      print(movimento, 'parado!')
+      self.cor = cor
+      self.marcar = marca
+      self.modelo = modelo
+      self.ligado = False
       self.andando = False
-    
-    def status(self):
-        motor = f'o motor esta ligado: ? {self.ligado}.' 
-        
-        
-        movimento =f'o carro esta em movimento? {self.andando} '
 
-        return motor + ' ' + movimento
+  def ligar(self):
+      if self.ligado:
+        print('o carro ja esta ligado')
+        return
 
-carro = Carro("cor", "marca", "modelo")
-carro.desligar()
-print(carro.status())
+      print('Ligado')
+      self.ligado = True 
 
-carro.ligar()
+  def desligar(self):
+      if not self.ligado:
+        print('o carro ja esta desligado')
+        return 
+      print('desligado')
+      self.ligado = False
+  
+  def andar(self):
+      if self.ligado == False:
+        print('é preciso ligar o carro primeiro!')
+        return
 
-    
-    
+      print('esta andando')
+      self.andando = True
+
+  def parar(self):
+      if not self.andando:
+        print('o carro ja esta parado!')
+        return
+
+      print('parado!')  
+      self.andando = True
+
+  def status_motor(self):
+      if self.ligado == True: 
+        print(f'o motor esta ligado?:{self.ligado} ')
+      else:
+        print(f'o motor esta ligado?: {self.ligado}')
+
+  
 
 
- 
+      
+
+
+c = Carro("cor","marca","modelo")
+c.ligar()
+c.andar()
+c.parar()
+c.desligar()
+
+
+print(c.status_motor())
